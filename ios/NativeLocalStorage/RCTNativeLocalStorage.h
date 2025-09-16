21a2944adb599b2f4a76ee36e20897362f206eeb
Brian Yeh <brian.yeh@coolbitx.com>
2025-09-16 17:31:30 +0800

    chore(ios): add RCTNativeLocalStorage

---
+++ ios/NativeLocalStorage/RCTNativeLocalStorage.h
@@ -0,0 +1,16 @@
+//
+//  RCTNativeLocalStorage.h
+//  TurboModuleExample
+//
+//  Created by Brian Yeh on 2025/9/16.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RCTNativeLocalStorage : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ios/NativeLocalStorage/RCTNativeLocalStorage.mm
@@ -0,0 +1,12 @@
+//
+//  RCTNativeLocalStorage.m
+//  TurboModuleExample
+//
+//  Created by Brian Yeh on 2025/9/16.
+//
+
+#import "RCTNativeLocalStorage.h"
+
+@implementation RCTNativeLocalStorage
+
+@end
